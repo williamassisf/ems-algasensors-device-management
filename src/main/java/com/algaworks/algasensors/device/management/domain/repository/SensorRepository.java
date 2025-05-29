50c0a11d46df74b3aca1b9bb31f9c08cc8ac35a8
william <williamassisf@gmail.com>
2025-05-28 22:09:05 -0300

    "Adicionar `SensorRepository` como repositório JPA para a entidade `Sensor` e implementar `SensorId` como identificador embutido, com suporte a TSID no microserviço de gerenciamento de dispositivos."

---
+++ src/main/java/com/algaworks/algasensors/device/management/domain/repository/SensorRepository.java
@@ -0,0 +1,10 @@
+package com.algaworks.algasensors.device.management.domain.repository;
+
+import com.algaworks.algasensors.device.management.domain.model.Sensor;
+import com.algaworks.algasensors.device.management.domain.model.SensorId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SensorRepository extends JpaRepository<Sensor, SensorId> {
+}
