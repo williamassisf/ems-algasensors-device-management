2b9172a864a2288db56285764e5672ae099b3f66
william <williamassisf@gmail.com>
2025-05-28 22:09:26 -0300

    "Adicionar conversor JPA para TSID (`TSIDToLongJPAAttributeConverter`) e persistir sensores no banco de dados, utilizando `SensorRepository` no controlador `SensorController`."

--- src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -3,24 +3,32 @@ package com.algaworks.algasensors.device.management.api.controller;
 import com.algaworks.algasensors.device.management.api.model.SensorInput;
 import com.algaworks.algasensors.device.management.common.IdGenerator;
 import com.algaworks.algasensors.device.management.domain.model.Sensor;
+import com.algaworks.algasensors.device.management.domain.model.SensorId;
+import com.algaworks.algasensors.device.management.domain.repository.SensorRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/sensors")
+@RequiredArgsConstructor
 public class SensorController {
+
+	private final SensorRepository repository;
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Sensor createSensor(@RequestBody SensorInput input) {
-		return Sensor.builder()
-					 .id(IdGenerator.generateTSID())
-					 .name(input.getName())
-					 .ip(input.getIp())
-					 .location(input.getLocation())
-					 .protocol(input.getProtocol())
-					 .model(input.getModel())
-					 .enabled(false)
-					 .build();
+		var sensor = Sensor.builder()
+						   .id(new SensorId(IdGenerator.generateTSID()))
+						   .name(input.getName())
+						   .ip(input.getIp())
+						   .location(input.getLocation())
+						   .protocol(input.getProtocol())
+						   .model(input.getModel())
+						   .enabled(false)
+						   .build();
+
+		return repository.save(sensor);
 	}
 }
